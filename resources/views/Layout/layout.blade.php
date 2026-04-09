43ca05b20e8bb2c09ddcb3da4900a78e91a8aa5d
ARTHUR LIMA DA SILVA <arthur.silva2008@cs.cruzeirodosul.edu.br>
2026-04-09 19:05:35 -0300

    style: centraliza o conteúdo principal de todas as views

--- resources/views/Layout/layout.blade.php
+++ resources/views/Layout/layout.blade.php
@@ -9,11 +9,11 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body x-data="{}">
+<body x-data="{}" class="h-screen">
     <!-- Header Component --->
     <x-header></x-header>
 
-    <main>
+    <main class="flex flex-col items-center w-full p-4">
         @yield('content')
     </main>
 </body>
