6e4e3d4af5d8c8d6fb5064389dc7333c06e924eb
Arthurlima0301 <arthur.limasilva2008@gmail.com>
2026-04-10 11:03:26 -0300

    feat(IN-64): adiciona component card e imagem do botão de retorno

---
+++ resources/views/Components/card.blade.php
@@ -0,0 +1,13 @@
+<div class="flex justify-between items-center w-full p-4 border-2 border-gray-300 shadow-md rounded-md">
+    <div class="flex items-center gap-3">
+        <button onclick="window.history.back()">
+            <img src="{{ asset('/img/buttons/btn-return.svg') }}" class="h-9 cursor-pointer" alt="">
+        </button>
+
+        <h1 class="text-xl font-bold">{{ $title }}</h1>
+    </div>
+
+    <div>
+        {{ $slot }}
+    </div>
+</div>
