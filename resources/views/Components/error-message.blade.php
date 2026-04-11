b8c517e927f607d99f6356b024e3babbaf8534e6
Arthurlima0301 <arthur.limasilva2008@gmail.com>
2026-04-11 18:39:01 -0300

    refactor: melhoria visual da mensagens de erro e sucesso e remoção ao clicar na tela

--- resources/views/Components/error-message.blade.php
+++ resources/views/Components/error-message.blade.php
@@ -3,7 +3,7 @@
 @endphp
 
 @if ($errorMessage)
-    <div class="flex justify-center items-center gap-3 p-4 bg-danger rounded-md text-muted text-lg">
+    <div class="flex justify-center items-center gap-3 w-full p-4 m-3 bg-danger rounded-md text-muted text-lg" @click.outside="$el.remove()">
         <img src="{{ asset('/img/icons/icon-error.svg') }}" alt="Error" class="h-10">
         <p>{{ $errorMessage }}</p>
     </div>
