f6f850b0d95901658ed39c7e7ca241e578a630b6
Arthurlima0301 <arthur.limasilva2008@gmail.com>
2026-04-11 19:43:44 -0300

    style(IN-65): cria component form e input

---
+++ resources/views/Components/form.blade.php
@@ -0,0 +1,15 @@
+<form action="{{ $action }}" method="{{ $method }}"
+    class="flex flex-col gap-6 justify-center items-center h-[40rem] w-full bg-surface rounded-lg shadow-md border-2 border-gray-300"
+>
+    @csrf
+
+    <h1 class="text-[30px] font-bold ">{{ $title ?? 'Formulário' }}</h1>
+
+    <div class="flex flex-col gap-4">
+        {{ $slot }}
+    </div>
+
+    <button type="submit" class="min-w-[300px] bg-primary text-muted p-3 rounded-md cursor-pointer">
+        {{ $buttonText ?? 'Enviar' }}
+    </button>
+</form>
---
+++ resources/views/Components/input.blade.php
@@ -0,0 +1,4 @@
+<div class="flex flex-col gap-2">
+    <label for="{{ $name }}">{{ $label }}</label>
+    <input type="text" name="{{ $name }}" id="{{ $name }}" class="min-w-[300px] border border-gray-300 rounded-md p-2" required>
+</div>
