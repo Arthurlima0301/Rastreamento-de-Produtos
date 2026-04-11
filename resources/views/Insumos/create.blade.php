3cf11e6d283a44a2b11ea3112bc7357307c48406
Arthurlima0301 <arthur.limasilva2008@gmail.com>
2026-04-11 19:44:37 -0300

    refactor(IN-65): altera html puro por component form e input

--- resources/views/Insumos/create.blade.php
+++ resources/views/Insumos/create.blade.php
@@ -3,28 +3,15 @@
 @section('title', 'Criar Insumo')
 
 @section('content')
-<x-card title="Criar Insumo">
-    <x-slot name="slot">
-        <!-- action slot left intentionally empty -->
-    </x-slot>
-</x-card>
+<x-card title="Criar Insumo"></x-card>
 
 <x-error-message></x-error-message>
 
-<form action="{{ route('insumos.store') }}" method="POST">
-    @csrf
-    <div>
-        <label for="codigo_insumo">Código:</label>
-        <input type="text" name="codigo_insumo" id="codigo_insumo" required>
-    </div>
-    <div>
-        <label for="nome">Nome:</label>
-        <input type="text" name="nome" id="nome" required>
-    </div>
-    <div>
-        <label for="unidade_medida">Unidade de Medida:</label>
-        <input type="text" name="unidade_medida" id="unidade_medida" required>
-    </div>
-    <button type="submit">Criar Insumo</button>
-</form>
+<x-form action="{{ route('insumos.store') }}" method="POST" title="Criar Insumo" buttonText="Criar Insumo">
+
+    <x-input name="codigo_insumo" label="Código" type="text" required></x-input>
+    <x-input name="nome" label="Nome" type="text" required></x-input>
+    <x-input name="unidade_medida" label="Unidade de Medida" type="text" required></x-input>
+</x-form>
+
 @endsection
