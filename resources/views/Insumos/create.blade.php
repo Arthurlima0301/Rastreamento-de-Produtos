d5f7b2b70e1452f2482ada0eb3cf4d64e62c0dc0
Arthurlima0301 <arthur.limasilva2008@gmail.com>
2026-04-11 21:49:47 -0300

    fix: ajustando erro de 'undefined $value' do component

--- resources/views/Insumos/create.blade.php
+++ resources/views/Insumos/create.blade.php
@@ -9,9 +9,9 @@
 
 <x-form action="{{ route('insumos.store') }}" method="POST" title="Criar Insumo" buttonText="Criar Insumo">
 
-    <x-input name="codigo_insumo" label="Código" type="text" required></x-input>
-    <x-input name="nome" label="Nome" type="text" required></x-input>
-    <x-input name="unidade_medida" label="Unidade de Medida" type="text" required></x-input>
+    <x-input name="codigo_insumo" label="Código" type="text" value="" required></x-input>
+    <x-input name="nome" label="Nome" type="text" value="" required></x-input>
+    <x-input name="unidade_medida" label="Unidade de Medida" type="text" value=""  required></x-input>
 </x-form>
 
 @endsection
