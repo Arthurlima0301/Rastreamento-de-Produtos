551fdd239f84a3f89eb1358b0741a30001c8daef
Arthurlima0301 <arthur.limasilva2008@gmail.com>
2026-04-14 22:47:39 -0300

    feat(IN-72): adiciona append 'saldo' ao model Item

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -16,13 +16,37 @@ class Item extends Model
         'quantidade',
     ];
 
+    /**
+     * The balance append to the model.
+     */
+    protected $appends = ['saldo'];
+
+    public function getSaldoAttribute()
+    {
+        return $this->quantidade - $this->saidas_items_sum_quantidade;
+    }
+
+    /**
+     * Get the nota fiscal that owns the item.
+     */
     public function notaFiscal()
     {
         return $this->belongsTo(NotaFiscal::class, 'nota_fiscal_id');
     }
 
+    /**
+     * Get the insumo that owns the item.
+     */
     public function insumo()
     {
         return $this->belongsTo(Insumo::class, 'insumo_id');
+    }
+
+    /**
+     * Get the saida items for the item.
+     */
+    public function saidasItems()
+    {
+        return $this->hasMany(SaidaItem::class, 'item_id');
     }
 }
