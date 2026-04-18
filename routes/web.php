cbb728148b81c0960ca15942d65b53c3f0e96cf9
Arthurlima0301 <arthur.limasilva2008@gmail.com>
2026-04-17 22:45:03 -0300

    chore: remove default welcome view and update web routes (insumos resource, saídas create/store)

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,17 @@ use App\Http\Controllers\ItemController;
 use App\Http\Controllers\NotaFiscalController;
 use App\Http\Controllers\SaidaController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
+/** */
 Route::resource('insumos', InsumoController::class);
 
 Route::get('/notas', [NotaFiscalController::class, 'index'])->name('notas.index');
 Route::post('notas/import', [NotaFiscalController::class, 'import'])->name('notas.import');
 
+/** */
 Route::get('/items', [ItemController::class, 'index'])->name('items.index');
 Route::get('/saidas', [SaidaController::class, 'index'])->name('saidas.index');
+
+
+Route::get('/saidas/create', [SaidaController::class, 'create'])->name('saidas.create');
+Route::post('/saidas', [SaidaController::class, 'store'])->name('saidas.store');
