7f17db1c97d546684a2f46dfe2c4aeb00e003596
Arthurlima0301 <arthur.limasilva2008@gmail.com>
2026-05-02 12:26:58 -0300

    refactor: mover validação de XML para rule

--- app/Services/ImportInvoiceFromXMLService.php
+++ app/Services/ImportInvoiceFromXMLService.php
@@ -15,33 +15,10 @@ class ImportInvoiceFromXMLService
         $xml = simplexml_load_file($file->getRealPath());
 
         DB::transaction(function () use ($xml) {
-            $this->validateFields($xml);
-            $this->validateAlreadyExists($xml);
-
             $invoice = $this->createInvoice($xml);
 
             $this->importItems($xml, $invoice->id);
         });
-    }
-
-    /**
-     * Validate if the XML file contains the necessary fields before importing.
-     */
-    private function validateFields($xml)
-    {
-        if (!isset($xml->NFe->infNFe->ide->nNF) || !isset($xml->NFe->infNFe->ide->dhEmi)) {
-            throw new \Exception('O XML não contém os campos necessários: nNF e dhEmi.');
-        }
-    }
-
-    /**
-     * Validate the XML file already exists.
-     */
-    private function validateAlreadyExists($xml)
-    {
-        if (Invoice::where('invoice_code', $xml->NFe->infNFe->ide->nNF)->exists()) {
-            throw new \Exception('Nota fiscal já existe.');
-        }
     }
 
     /**
@@ -60,6 +37,6 @@ class ImportInvoiceFromXMLService
      */
     private function importItems($xml, int $invoiceId)
     {
-        (new ExtractItems())->extract($xml, $invoiceId);
+        (new ExtractItems)->extract($xml, $invoiceId);
     }
 }
