dedfb8dc0f71551c2d157f3e9e4edd2b4f109e45
Arthurlima0301 <arthur.limasilva2008@gmail.com>
2026-06-02 11:37:12 -0300

    refactor: melhorar vizualização de opções do menu

--- resources/views/Components/menu.blade.php
+++ resources/views/Components/menu.blade.php
@@ -7,17 +7,18 @@
         </flux:menu.group>
         <flux:menu.group heading="Material e Bobinas">
             <flux:menu.item href="{{ route('orders.index') }}" icon="clipboard">Ordens de Corte</flux:menu.item>
-            <flux:menu.item href="{{ route('material-invoices.index') }}" icon="document-text">Notas Fiscais de Materiais
+            <flux:menu.item href="{{ route('material-invoices.index') }}" icon="document-text">Notas Fiscais
             </flux:menu.item>
-            <flux:menu.item href="{{ route('item-materials.index') }}" icon="rectangle-stack">Itens de Material
+            <flux:menu.item href="{{ route('item-materials.index') }}" icon="rectangle-stack">Itens
             </flux:menu.item>
         </flux:menu.group>
         <flux:menu.group heading="Insumos">
-            <flux:menu.item href="{{ route('supply-invoices.index') }}" icon="document-text">Notas Fiscais de Insumos
-            </flux:menu.item>
-            <flux:menu.item href="{{ route('supply-items.index') }}" icon="rectangle-stack">Itens de Insumo
-            </flux:menu.item>
             <flux:menu.item href="{{ route('supplies.index') }}" icon="cube">Insumos</flux:menu.item>
+            <flux:menu.item href="{{ route('supply-invoices.index') }}" icon="document-text">Notas Fiscais  
+            </flux:menu.item>
+            <flux:menu.item href="{{ route('supply-items.index') }}" icon="rectangle-stack">Itens 
+            </flux:menu.item>
+       
         </flux:menu.group>
         <flux:menu.group heading="Relatórios">
             <flux:menu.item href="{{ route('dispatches.index') }}" icon="truck">Saídas</flux:menu.item>
