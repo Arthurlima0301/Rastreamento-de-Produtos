5d257559f022a0d909a3b4636c93ed7ad6232c91
Arthurlima0301 <arthur.limasilva2008@gmail.com>
2026-06-06 19:19:13 -0300

    fix: impossibilitar exclusão de máquina se associada a cargas

--- app/Livewire/Machines/MachineTable.php
+++ app/Livewire/Machines/MachineTable.php
@@ -22,10 +22,15 @@ class MachineTable extends Component
         return view('livewire.machines.machine-table', compact('machines'));
     }
 
-    public function destroy(int $machineId)
+    public function destroy(Machine $machine)
     {
-        Machine::findOrFail($machineId)->delete();
+        if ($machine->loads()->exists()) {
+            return redirect()
+                ->route('machines.index')
+                ->with('error', 'Não é possível deletar esta máquina, pois ela está associada a um ou mais cargas.');
+        }
 
+        $machine->delete();
         return redirect()->route('machines.index')->with('success', 'Máquina deletada com sucesso.');
     }
 }
