8ae1389d0af0b65902f8334cd5b56816b58dc1ce
Arthurlima0301 <arthur.limasilva2008@gmail.com>
2026-06-08 20:32:35 -0300

    feat: ver todas as cargas associadas a uma máquina

---
+++ app/Livewire/Machines/MachineShow.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Livewire\Machines;
+
+use App\Models\Load;
+use App\Models\Machine;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
+use Livewire\Component;
+
+#[Layout('Layout.layout')]
+#[Title('Detalhes da Máquina')]
+class MachineShow extends Component
+{
+    public Machine $machine;
+
+    public function mount(Machine $machine)
+    {
+        $this->machine = $machine;
+    }
+
+    public function render()
+    {
+        $loads = Load::withSum('rolls','weight')
+        ->withCount('rolls')
+        ->orderBy('cutted_at', 'desc')
+        ->where('machine_id', $this->machine->id)
+        ->paginate(50);
+        return view('livewire.machines.machine-show', compact('loads'));
+    }
+}
