a4f9ec8aa8f0cc6206e4814fa1ee9ba0d1089d95
Arthurlima0301 <arthur.limasilva2008@gmail.com>
2026-06-12 23:36:06 -0300

    feat: trocar tema do sistema

--- resources/views/Components/header.blade.php
+++ resources/views/Components/header.blade.php
@@ -1,3 +1,17 @@
-<header class="w-full p-4 bg-main">
+<header class="flex justify-between items-center w-full p-4" x-data>
     <x-menu></x-menu>
+
+    <flux:dropdown>
+        <x-button icon="ellipsis-horizontal" />
+
+        <flux:menu>
+            <flux:menu.item 
+                class="cursor-pointer" 
+                icon="moon" 
+                x-on:click="$flux.appearance = $flux.appearance === 'light' ? 'dark' : 'light'"
+            >
+                Trocar Tema
+            </flux:menu.item>
+        </flux:menu>
+    </flux:dropdown>
 </header>
