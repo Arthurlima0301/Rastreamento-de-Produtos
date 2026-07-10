6339cfb775e086bc84ad1085fa20759e8c6dca11
Arthurlima0301 <arthur.limasilva2008@gmail.com>
2026-07-10 00:21:50 -0300

    fix: ajuste para exibir corretamente a quantidade de pallets bobinas especifica de cada item material

--- app/Livewire/ItemMaterials/ItemMaterialLoads.php
+++ app/Livewire/ItemMaterials/ItemMaterialLoads.php
@@ -13,24 +13,25 @@ class ItemMaterialLoads extends Component
     /**
      * 
      */
+    public function mount(ItemMaterial $itemMaterial)
+    {
+        $this->itemMaterial = $itemMaterial;
+    }
 
     /**
      * Render the component view.
      */
     public function render()
     {
-        $loads = Load::whereHas('rolls', function ($query) {
-            $query->where('item_material_id', $this->itemMaterial->id);
-        })
+        $loads = Load::query()
         ->with('machine')
-        ->withSum(['rolls as total_rolls_weight' => function ($query) {
-            $query->where('item_material_id', $this->itemMaterial->id);
-        }], 'weight')
-        ->withSum(['pallets as total_pallets_weight' => function ($query) {
-            $query->where('item_material_id', $this->itemMaterial->id);
-        }], 'package_net_weight')
-        ->withCount('rolls as total_rolls')
-        ->withCount('pallets as total_pallets')
+        ->withSum(['rolls as total_rolls_weight' => fn ($q) => $q->where('item_material_id', $this->itemMaterial->id)], 'weight')
+        ->withSum(['pallets as total_pallets_weight' => fn ($q) => $q->where('item_material_id', $this->itemMaterial->id)], 'package_net_weight')
+        ->withCount(['rolls as total_rolls' => fn ($q) => $q->where('item_material_id', $this->itemMaterial->id)])
+        ->withCount(['pallets as total_pallets' => fn ($q) => $q->where('item_material_id', $this->itemMaterial->id)])
+        ->whereHas('rolls', function ($q) {
+            $q->where('item_material_id', $this->itemMaterial->id);
+        })
         ->paginate(50);
 
         return view('livewire.item-materials.item-material-loads', compact('loads'));
